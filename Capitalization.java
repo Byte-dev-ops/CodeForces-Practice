239ecc99d7bcf6d90cc6941eb8a23d0682a77d00
Byte Dev Ops <mukul.mutreja348@gmail.com>
2026-07-22 23:59:29 +0530

    Add Capitalization.java for string capitalization

---
+++ Capitalization.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        String result = str.substring(0, 1).toUpperCase() + str.substring(1);
+
+        System.out.println(result);
+    }
+}
