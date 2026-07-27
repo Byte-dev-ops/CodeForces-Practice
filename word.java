e03036e3faa0e35fc304266b05bb987cc69955c4
Byte Dev Ops <mukul.mutreja348@gmail.com>
2026-07-27 12:12:58 +0530

    Add Java program to convert case based on counts

---
+++ word.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        int lower = 0;
+        int upper = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isLowerCase(s.charAt(i))) {
+                lower++;
+            } else {
+                upper++;
+            }
+        }
+
+        if (upper > lower) {
+            System.out.println(s.toUpperCase());
+        } else {
+            System.out.println(s.toLowerCase());
+        }
+    }
+}
