137a4c1f138c0dd307e946e6134c849409facd45
Byte Dev Ops <mukul.mutreja348@gmail.com>
2026-07-29 17:54:10 +0530

    Add palindrome check in translation.java

---
+++ translation.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String t = sc.next();
+        String reversed = new StringBuilder(s).reverse().toString();
+        if(t.equals(reversed)){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
